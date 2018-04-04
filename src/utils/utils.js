dd9eac88772c12624bc78a73ada34b00382480de
father-gregor <benlinus92@gmail.com>
2018-04-05 02:05:37 +0300

    Utils module for helper functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,8 +2,16 @@
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-  }
+}
+
+function timer (start) {
+    if (!start)
+        return process.hrtime();
+    let end = process.hrtime(start);
+    return Math.round((end[0]*1000) + (end[1]/1000000));
+};
 
 module.exports = {
-    getRandomInt
+    getRandomInt,
+    timer
 }
